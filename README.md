413d3dc2ef2feaa068e2a2d0fbf84ead2cc13bbd
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-25 18:25:40 +0200

    Stampa a video delle card progetto

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ nome repo 2: `vite-boolfolio`
 - [x] Colleghiamo questo progetto ad una repo separata.
 
 **Milestone 4**  
-- [ ] Nel componente principale della nostra Vue App facciamo una chiamata API all’endpoint costruito nel progetto Laravel (milestone 1) e recuperiamo tutti i progetti dal nostro back-end.  
-- [ ] Stampiamo in console i risultati e verifichiamo di ricevere i dati correttamente.
+- [x] Nel componente principale della nostra Vue App facciamo una chiamata API all’endpoint costruito nel progetto Laravel (milestone 1) e recuperiamo tutti i progetti dal nostro back-end.  
+- [x] Stampiamo in console i risultati e verifichiamo di ricevere i dati correttamente.
 
 **Milestone 5**  
 - [ ] Creiamo un nuovo componente ProjectCard, che corrisponde ad una card per visualizzare un progetto. Utilizziamo questo componente per visualizzare tutti i progetti ricevuti tramite API.
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -4,11 +4,17 @@
             <ul class="list-unstyled row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 gy-4">
                 <li class="col col-md-4" v-for=" project in data">
                     <div class="card">
-                        <img v-if="project.immagine" src="..." class="card-img-top" alt="...">
+                        <img v-if="project.immagine" :src="project.immagine" class="card-img-top" :alt="project.nome">
                         <div class="card-body">
-                            <h5 class="card-title">{{ project.nome }}</h5>
+                            <h2 class="card-title">{{ project.nome }}</h2>
                             <p v-if="project.descrizione" class="card-text">{{ project.descrizione.substring(0,200) + '...' }}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="my-3">Data: {{ project.data_di_creazione }}</div>
+                            <div class="fs-6 fw-bold">Completato</div>
+                            <div :class="[project.completato ? 'completato' : 'lavoro']"></div>
+                            <h6 v-if="project.type"><span class="badge bg-primary p-2">{{ project.type.nome }}</span></h6>
+                            <div class="d-flex gap-2" v-if=" project.technologies != 0">
+                                <h6 v-for="technologies in project.technologies"><span class="badge bg-dark p-2">{{ technologies.nome}}</span></h6>
+                            </div>
                         </div>
                     </div>
                 </li>
@@ -29,5 +35,19 @@ export default {
 main {
     background-color: #efefef;
     padding: 30px 0;
+}
+.completato{
+    width: 15px;
+    height: 15px;
+    margin: 10px 0;
+    border-radius: 50%;
+    background-color: green;
+}
+.lavoro{
+    width: 15px;
+    height: 15px;
+    margin: 10px 0;
+    border-radius: 50%;
+    background-color: red;
 }
 </style>
